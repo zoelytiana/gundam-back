bceb5450e472eccda3fb5ef957786dff75b9059b
Amanda Lan-Cheong <amanda.lan-cheong@gmail.com>
2021-11-30 11:03:41 +0100

    cors

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const express = require("express");
 const routesProducts = require("./routes/index.js")
 const routesUsers = require("./routes/user.js")
@@ -9,14 +9,14 @@ const server = express();
 
 server.use(cors());
 
-server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+// server.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//     next();
+//   });
 
-server.use(bodyParser.json());
+// server.use(bodyParser.json());
 
 routesProducts(server);
 routesUsers(server);
