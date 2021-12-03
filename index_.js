feec862084d060159eb967275f59274d9ef4146f
Zoely <zralamboarisoa@gmail.com>
2021-12-03 11:49:58 +0100

    order route controller model

---
+++ index_.js
@@ -0,0 +1,28 @@
+const bodyParser = require('body-parser');
+const express = require("express");
+const routesProducts = require("./routes/index.js")
+const routesUsers = require("./routes/user.js")
+require("dotenv").config({path:"./config/.env"});
+require("./config/db");
+//const cors = require("cors")
+const server = express();
+
+//server.use(cors());
+
+ server.use((req, res, next) => {
+     res.setHeader('Access-Control-Allow-Origin', '*');
+     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+     next();
+   });
+
+ server.use(bodyParser.json());
+
+routesProducts(server);
+routesUsers(server);
+
+server.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);    
+})
+
+
