2a8946485a2a56a4699d5255bde9efcbb9de4dc4
Vincent-lebrun <lebrunvincent12@gmail.com>
2021-12-03 11:25:41 +0100

    push for zoely

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-// const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const express = require("express");
-const routesProducts = require("./routes/index.js")
-const routesUsers = require("./routes/user.js")
-require("dotenv").config({path:"./config/.env"});
+const routesProducts = require("./routes/index.js");
+const routesUsers = require("./routes/user.js");
+const routesOrders = require("./routes/order.js");
+require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
-const cors = require("cors")
+// const cors = require("cors");
 const server = express();
 
-server.use(cors());
+// server.use(cors());
 
-// server.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//     next();
-//   });
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
-// server.use(bodyParser.json());
-
+server.use(bodyParser.json());
+routesOrders(server);
 routesProducts(server);
 routesUsers(server);
 
 server.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);    
-})
+  console.log(`Listening on port ${process.env.PORT}`);
+});
 
 server.get("/test", (req, res) => {
-    res.send({
-        result: "Petit test de tout ça laaaaaaa"
-    })
-})
+  res.send({
+    result: "Petit test de tout ça laaaaaaa",
+  });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 const ProductController = require("../controllers/product.js");
 
 module.exports = (server) => {
-    //AFFICHER LES PRODUITS
-    server.get("/products", (req, res) => {
-        ProductController.getAll(req, res);
-    })
-    //AFFICHER LES DETAILS DES PRODUITS AVEC UN ID
-    server.get("/products/:_id", (req, res) => {
-        ProductController.getId(req, res);
-    })
-}
-
+  //AFFICHER LES PRODUITS
+  server.get("/products", (req, res) => {
+    ProductController.getAll(req, res);
+  });
+  //AFFICHER LES DETAILS DES PRODUITS AVEC UN ID
+  server.get("/products/:_id", (req, res) => {
+    ProductController.getId(req, res);
+  });
+};
